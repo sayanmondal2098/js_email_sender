df90520b29479d694ef19e6177e942940668039c
Sayan <sayanmondal2098@gmail.com>
2019-05-04 23:47:50 +0530

    added navigator pages

---
+++ src/components/backup.js
@@ -0,0 +1,173 @@
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/core/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Settings from "./Settings";
+import SendEmail from "./SendEmail";
+import SendEmailBulk from "./SendEmailBulk";
+
+const Welcome = () => {
+  return <h1>Welcome to the APP!</h1>;
+};
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`
+    }
+  },
+  menuButton: {
+    marginRight: 20,
+    [theme.breakpoints.up("sm")]: {
+      display: "none"
+    }
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3
+  }
+});
+
+class SideBar extends React.Component {
+  state = {
+    mobileOpen: false
+  };
+
+  handleDrawerToggle = () => {
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+  };
+
+  handlePassword = () => {
+    this.setState({ show: true });
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+
+    const drawer = (
+      <div>
+        <div className={classes.toolbar} />
+        <Divider />
+
+        <List>
+          <ListItem button key={"home"} component={Link} to="/">
+            <i className="material-icons">home</i>
+            <ListItemText primary={"Home"} />
+          </ListItem>
+          <ListItem button key={"send"} component={Link} to="/send">
+            <i className="material-icons">email</i>
+            <ListItemText primary={"Send Email"} />
+          </ListItem>
+          <ListItem button key={"sendbulk"} component={Link} to="/sendbulk">
+            <i className="material-icons">email</i>
+            <ListItemText primary={"Send Email Bulk"} />
+          </ListItem>
+          <ListItem button key={"setting"} component={Link} to="/settings">
+            <i className="material-icons">settings</i>
+            <ListItemText primary={"Settings"} />
+          </ListItem>
+        </List>
+      </div>
+    );
+
+    return (
+      <Router>
+        <div className={classes.root}>
+          <CssBaseline />
+          <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.handleDrawerToggle}
+                className={classes.menuButton}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit" noWrap>
+                Email Sender
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <nav className={classes.drawer}>
+            {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+            <Hidden smUp implementation="css">
+              <Drawer
+                container={this.props.container}
+                variant="temporary"
+                anchor={theme.direction === "rtl" ? "right" : "left"}
+                open={this.state.mobileOpen}
+                onClose={this.handleDrawerToggle}
+                classes={{
+                  paper: classes.drawerPaper
+                }}
+                ModalProps={{
+                  keepMounted: true // Better open performance on mobile.
+                }}
+              >
+                {drawer}
+              </Drawer>
+            </Hidden>
+            <Hidden xsDown implementation="css">
+              <Drawer
+                classes={{
+                  paper: classes.drawerPaper
+                }}
+                variant="permanent"
+                open
+              >
+                {drawer}
+              </Drawer>
+            </Hidden>
+          </nav>
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+
+            <Route path="/" exact component={Welcome} />
+            <Route path="/settings/" component={Settings} />
+            <Route path="/send/" component={SendEmail} />
+            <Route path="/sendbulk/" component={SendEmailBulk} />
+          </main>
+        </div>
+      </Router>
+    );
+  }
+}
+
+SideBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+  // Injected by the documentation to work in an iframe.
+  // You won't need it on your project.
+  container: PropTypes.object,
+  theme: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, { withTheme: true })(SideBar);
